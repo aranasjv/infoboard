95f94a1038b5bd5caf0e456a06197c01afa7454d
aranasjv <aranasjv@gmail.com>
2019-02-25 00:29:12 +0800

    'finalsuper'

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -5,8 +5,8 @@
 
 
     <div class="row">
-        <div class="col-xs-2"></div>
-        <div class="col-xs-8">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
             <!---Welcome---->
             <div class="container-fluid">
                 <div class="panel panel-default">
@@ -20,14 +20,14 @@
                 </div>
             </div>
         </div>
-        <div class="col-xs-2"></div>
+        <div class="col-sm-1"></div>
     </div>
 
 
 
     <div class="row">
-        <div class="col-xs-2"></div>
-        <div class="col-xs-8">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10">
             <!-- announcement -->
             <div class="container-fluid">
                 <div class="col">
@@ -45,14 +45,14 @@
                 </div>
             </div>
             </div>
-        <div class="col-xs-2"></div>
+        <div class="col-sm-1"></div>
     </div>
 
 
 
     <div class="row">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-8"> <!-- Video Post -->
+        <div class="col-sm-1"></div>
+        <div class="col-sm-10"> <!-- Video Post -->
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col">
@@ -89,7 +89,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-2"></div>
+        <div class="col-sm-1"></div>
     </div>
 
 
