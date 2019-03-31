8f633998f2f4956f1fdc1d7d6b65eb5e019abe58
aranasjv <aranasjv@gmail.com>
2019-03-31 21:58:09 +0800

    'revision15'

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -11,8 +11,7 @@
             <script src={{asset('js/lib/jquery.min.js')}}></script>
             <script src={{asset('js/fullcalendar.min.js')}}></script>
             <script src='../fullcalendar.min.js'></script>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
             <script type="text/javascript" href="https://cdnjs.cloudflare.com/ajax/libs/FitText.js/1.1/jquery.fittext.js"></script>
 
             <meta charset="utf-8">
