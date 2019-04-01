a51c93cba80835453e0077ecc0daa492fa3c8b36
aranasjv <aranasjv@gmail.com>
2019-04-01 22:22:35 +0800

    'revision16'

--- resources/views/frontend/singleEvent.blade.php
+++ resources/views/frontend/singleEvent.blade.php
@@ -1,4 +1,4 @@
-@extends('frontend.layouts.app')
+@extends('frontend.layouts.app1')
 
 @section('title','Event')
 
@@ -12,7 +12,7 @@
 
     <hr>
     <center><h2>Event Detail:</h2></center>
-    <center><h3>{!! $event->content !!}</h3></center>
+    <center><p>{!! $event->content !!}</p></center>
     <hr>
     <center><a href="/" class="btn btn-lg active" role="button" aria-pressed="true" style="background-color:darkred;"><font color="white">Back</font></a></center>
     <hr>
--- resources/views/frontend/singlePost.blade.php
+++ resources/views/frontend/singlePost.blade.php
@@ -13,7 +13,7 @@
     <hr>
     <center><img class="img-responsive" src="{{ asset('storage/img/blog/' . $blog->featured_image) }}"  alt="Card image cap"></center>
     <hr>
-    <center><h3>{!! $blog->content !!}</h3></center>
+    <center><p>{!! $blog->content !!}</p></center>
     <hr>
     <center><a href="/" class="btn btn-lg active" role="button" aria-pressed="true" style="background-color:darkred;"><font color="white">Back</font></a></center>
     <hr>
